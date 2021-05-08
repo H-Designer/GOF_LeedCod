b452c9e4dd5b852af955b4632ced2a5b0257bbb1
赵贤 <867595804@qq.com>
2021-05-08 10:14:24 +0800

    2021-5-8

--- src/LeeCode/LeeCode1022.java
+++ src/LeeCode/LeeCode1022.java
@@ -45,6 +45,37 @@ package LeeCode;
  * */
 public class LeeCode1022 {
 
+    /**
+     Definition for a binary tree node.
 
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Solution {
+        public int sumRootToLeaf(TreeNode root) {
+            int sum = 0;
+            return recursionSum(root, sum);
+        }
+        public int recursionSum(TreeNode treeNode, int sum){
+            if (treeNode==null){
+                return 0;
+            }
+            sum = sum * 2 + treeNode.val;
+            if (treeNode.left != null || treeNode.right != null){
+                return recursionSum(treeNode.left,sum) + recursionSum(treeNode.right, sum);
+            }
+            return sum;
+        }
+    }
 
 }
